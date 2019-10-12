e18fea7fc124715b7bbe9ec1cd319293e432da5c
Jonathan <jounanmin@hotmail.com>
2019-10-12 00:24:13 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,69 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+class TrafficLight extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			red: false,
+			yellow: true,
+			green: false
+		};
+	}
+
+	render() {
+		let lightRed = "";
+		let lightYellow = "";
+		let lightGreen = "";
+		if (this.state.red === true) {
+			lightRed = "light";
+		}
+		if (this.state.yellow === true) {
+			lightYellow = "light";
+		}
+		if (this.state.green === true) {
+			lightGreen = "light";
+		}
+
+		return (
+			<div className="bigContainer">
+				<div
+					className={`red ${lightRed}`}
+					onClick={() =>
+						this.setState({
+							red: true,
+							yellow: false,
+							green: false
+						})
+					}>
+					Red
+				</div>
+				<div
+					className={`yellow ${lightYellow}`}
+					onClick={() =>
+						this.setState({
+							red: false,
+							yellow: true,
+							green: false
+						})
+					}>
+					Yellow
+				</div>
+				<div
+					className={`green ${lightGreen}`}
+					onClick={() =>
+						this.setState({
+							red: false,
+							yellow: false,
+							green: true
+						})
+					}>
+					Green
+				</div>
+			</div>
+		);
+	}
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
